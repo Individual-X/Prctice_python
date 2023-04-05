93f3509fe4776f7eb460cd654268fc2a8fae152e
Sajib Hossain <sajib.hossain.limon@gmail.com>
2023-04-05 15:44:16 +0600

    Income tax according to the chart

---
+++ income_Tax.py
@@ -0,0 +1,14 @@
+"""For example, suppose the taxable income is 45000 the income tax payable is
+10000*0% + 10000*10%  + 25000*20% = $6000."""
+
+income = int(input("Input amount of money to calculate the income tax: "))
+
+if income <= 10000:
+    incometax = 0
+    print("$",incometax)
+elif income > 10000 and income <= 20000:
+    incometax = 10000 * (10/100)
+    print("$",incometax)
+else:
+    incometax = (10000 * (10 / 100))  + ((income - 20000) *(20/100))
+    print("$",incometax)
